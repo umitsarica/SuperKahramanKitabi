47f525dcb491a771d657f16baa21c956b7de253c
Ümit SARICA <usarica@gmail.com>
2024-02-06 15:50:31 +0300

    tableview was added (tableview eklendi)

--- SuperKahramanKitabi/ViewController.swift
+++ SuperKahramanKitabi/ViewController.swift
@@ -7,13 +7,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+
     }
-
-
+        //NumberofRowsInsect.on -> kaç tane row olacak
+        //cellForRow atIndexpatch -> hücrenin içerisinde neler gösterilecek
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "Test"
+        return cell
+    }
+    
+    
 }
+
+
+
 
